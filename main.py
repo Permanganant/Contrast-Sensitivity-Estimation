621c337d41605d744d406769455bc9bdfc1e9944
Mert Alperen Beser <46824021+Permanganant@users.noreply.github.com>
2023-05-23 12:07:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from psychopy.hardware import keyboard
 import random
 import csv
 import pandas as pd
-
-
+import random
+import math
+import sys
 def createStim(list_c, list_sf):
-    x = (-2,2,2,-2)
-    y = (-2,-2,2,2)
+    
+    x = (-2,2,-2,2)
+    y = (2,2,-2,-2)
     
     i = 0
     grating_1 = visual.GratingStim(win=mywin, mask="circle", size=3, pos=[x[i],y[i]],sf = list_sf[0],contrast = list_c[0])
@@ -45,44 +47,52 @@ log_contrast = np.array([])
 log_sf = np.array([])
 chosen_gratings = np.array([])
 j = 0
-keyList=['y','n','q']
+keyList=['1','2','3','4','q']
 keys = []
-N_times = 1
+N_times = 2
 repeated_c = []
 repeated_c_copy = []
 df_dict = {}
-all_sf = np.linspace(0.1,1,10)
+all_sf = np.logspace(-1,2,10)
+np.random.shuffle(all_sf)
+#all_sf = all_sf.tolist()
+#all_sf = np.random.uniform(0.1,math.log(2),10)
+#all_sf = np.linspace(0.1,1,5)
 index = []
-c = np.linspace(0,1,10)
+c = np.logspace(-3,0,10)
+c = c.tolist()
+#c = np.linspace(0.05,1,10)
 
-for i in range(N_times):
-    repeated_c_copy = np.append(repeated_c_copy,c)
-    
+for i in range(N_times*10):
+    np.random.shuffle(c)
+    repeated_c = np.append(repeated_c,c)
+#np.random.shuffle(repeated_c)
 i = 0
 ans = []
 ctrs = []
 r_sf = []
-for rnd_sf in all_sf:
+
+while repeated_c.size:  
     i = i + 1
-    repeated_c = repeated_c_copy
     
-    while repeated_c.size:  
-        
+    for rnd_sf in all_sf:
+        rnd_sf = float(rnd_sf) + sys.float_info.epsilon
         rnd_c = float(random.choice(repeated_c))
         index_to_remove = np.argwhere(repeated_c == rnd_c)[0][0]
         repeated_c = np.delete(repeated_c, index_to_remove)
-        print(len(repeated_c))
-        #rnd_sf = float(np.random.rand(1))
+       
+        
         list_c = [0,0,0,rnd_c]
         list_sf = [1,1,1,rnd_sf]
         
         mapIndexPosition = list(zip(list_c, list_sf))
         random.shuffle(mapIndexPosition)
-        
         list_c, list_sf = zip(*mapIndexPosition)
-        
+        non_one_indices = [index for index, element in enumerate(list_sf) if element != int(1)]
+        get_key = non_one_indices[0] + 1
+        print("Position of the circle: " +str(get_key))
         [sf,cont] = createStim(list_c, list_sf)
-        
+        print("Remaining test: " + str(len(repeated_c)))
         msg_1 = "Repeat: " + str(j)
         msg_2 = "Spatial Freq: " + str(rnd_sf)
         title_msg1 = visual.TextStim(win=mywin, text=msg_1, pos=[-3, 8], height=0.5, color="black")
@@ -91,24 +101,26 @@ for rnd_sf in all_sf:
         title_msg2.draw()
         mywin.flip()
         
-        keys = event.waitKeys(keyList=['y','n','q'])
+        keys = event.waitKeys(keyList=['1','2','3','4','q'])
         check_key = keys[0]
         
+        
         if check_key in keyList:
-            print("flag")
+            if int(check_key) == int(get_key):
+                y_n = 'y'
+            else:
+                y_n = 'n'
             j = j + 1
             if check_key == 'q':
                 core.quit()
                 break
-            ans.append(check_key)
+            ans.append(y_n)
             ctrs.append(rnd_c)
             r_sf.append(rnd_sf)
             index.append(j)
             check_key = []
         df = pd.DataFrame ({'user_answer': ans, 'contrast_value': ctrs,'spatial_freq': r_sf},index)
-        
-        
         mywin.update()
-df.to_csv('my_data_df.csv', index=False)
+df.to_csv('my_data_df_2.csv', index=False)
 
   
